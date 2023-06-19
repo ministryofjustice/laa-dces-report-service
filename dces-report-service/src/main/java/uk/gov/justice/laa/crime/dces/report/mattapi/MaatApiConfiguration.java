df2bcda185d7b39e8b7b5ea41893f82225853177
Tomas <tomas.novellino@digital.justice.gov.uk>
2023-06-19 09:46:37 +0100

    updates to config files and load

--- dces-report-service/src/main/java/uk/gov/justice/laa/crime/dces/report/mattapi/MaatApiConfiguration.java
+++ dces-report-service/src/main/java/uk/gov/justice/laa/crime/dces/report/mattapi/MaatApiConfiguration.java
@@ -4,11 +4,16 @@ import jakarta.validation.constraints.NotNull;
 import lombok.Data;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.context.annotation.PropertySources;
 
 
 // TODO (DCES-25): make it possible to load configuration values from own config file
 @Data
 @Configuration
+@PropertySources(
+        @PropertySource(value = "file:/config/application.yaml", ignoreResourceNotFound = true)
+)
 @ConfigurationProperties(prefix = "maat-api")
 public class MaatApiConfiguration {
     /**
--- dces-report-service/src/test/java/uk/gov/justice/laa/crime/dces/report/mattapi/MaatApiConfigurationTest.java
+++ dces-report-service/src/test/java/uk/gov/justice/laa/crime/dces/report/mattapi/MaatApiConfigurationTest.java
@@ -1,19 +1,13 @@
 package uk.gov.justice.laa.crime.dces.report.mattapi;
 
-//import org.junit.Test;
 import org.junit.jupiter.api.Test;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
-import org.springframework.boot.test.context.ConfigDataApplicationContextInitializer;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.env.Environment;
-import org.springframework.test.context.ContextConfiguration;
-
-
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 
 @SpringBootTest
