3fe0285d86e1276c22afe96068f27318ee9a50c8
Tomas <tomas.novellino@digital.justice.gov.uk>
2023-07-04 18:12:55 +0100

    remove unused imports

--- dces-report-service/src/main/java/uk/gov/justice/laa/crime/dces/report/maatapi/MaatApiClientFactory.java
+++ dces-report-service/src/main/java/uk/gov/justice/laa/crime/dces/report/maatapi/MaatApiClientFactory.java
@@ -1,10 +1,7 @@
 package uk.gov.justice.laa.crime.dces.report.maatapi;
 
 import lombok.RequiredArgsConstructor;
-import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.stereotype.Component;
 import org.springframework.web.reactive.function.client.WebClient;
 import org.springframework.web.reactive.function.client.support.WebClientAdapter;
 import org.springframework.web.service.invoker.HttpServiceProxyFactory;
