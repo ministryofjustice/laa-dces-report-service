ca3dccf560e8705b33e53dcd05d3d51b4d063295
Tomas <tomas.novellino@digital.justice.gov.uk>
2023-07-13 13:24:32 +0100

    Updated integration tests for ContributionFilesService
    Added integration tests and coverage on all possible responses. Also factored out Contributions Client test to have them separated from the Contributions service test

---
+++ dces-report-service/src/test/java/uk/gov/justice/laa/crime/dces/report/client/ContributionFilesClientTest.java
@@ -0,0 +1,60 @@
+package uk.gov.justice.laa.crime.dces.report.client;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestInstance;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.junit.jupiter.api.function.Executable;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+import uk.gov.justice.laa.crime.dces.report.maatapi.exception.MaatApiClientException;
+import uk.gov.justice.laa.crime.dces.report.model.ContributionFilesResponse;
+
+import java.time.LocalDate;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.*;
+
+@SpringBootTest
+@ExtendWith(MockitoExtension.class)
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)
+@ActiveProfiles("test")
+class ContributionFilesClientTest {
+    private static final LocalDate startPeriod = LocalDate.of(2023, 1, 1);
+    private static final LocalDate finishPeriod = LocalDate.of(2023, 1, 31);
+
+    @Mock
+    ContributionFilesClient contributionFilesClient;
+
+
+    @Test
+    void givenValidDateLimitParams_whenContributionClientSendGetRequestIsInvoked_thenResponseDataModelIsReturned() {
+        ContributionFilesResponse expectedResponse = getMockedMaatApiResponseModel();
+
+        when(contributionFilesClient.sendGetRequest(any(), any()))
+                .thenReturn(expectedResponse);
+
+        Assertions.assertDoesNotThrow(mockSendRequestGetContributionFiles());
+        verify(contributionFilesClient, times(1)).sendGetRequest(startPeriod, finishPeriod);
+    }
+
+    @Test
+    void givenValidDateLimitParams_whenContributionClientSendGetRequestIsInvoked_thenExceptionIsThrown() {
+        when(contributionFilesClient.sendGetRequest(any(), any()))
+                .thenThrow(Mockito.mock(MaatApiClientException.class));
+
+        assertThrows(MaatApiClientException.class, mockSendRequestGetContributionFiles());
+    }
+
+    private ContributionFilesResponse getMockedMaatApiResponseModel() {
+        return new ContributionFilesResponse(List.of("XML1", "XML2"));
+    }
+    
+    private Executable mockSendRequestGetContributionFiles() {
+        return () -> contributionFilesClient.sendGetRequest(startPeriod, finishPeriod);
+    }
+}
---
+++ dces-report-service/src/test/java/uk/gov/justice/laa/crime/dces/report/service/ContributionFilesServiceTest.java
@@ -0,0 +1,85 @@
+package uk.gov.justice.laa.crime.dces.report.service;
+
+import com.github.tomakehurst.wiremock.junit5.WireMockTest;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.web.client.HttpServerErrorException;
+import org.springframework.web.reactive.function.client.WebClientResponseException;
+import uk.gov.justice.laa.crime.dces.report.maatapi.exception.MaatApiClientException;
+import uk.gov.justice.laa.crime.dces.report.model.ContributionFilesResponse;
+
+import java.time.LocalDate;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+@SpringBootTest
+@ActiveProfiles("test")
+@WireMockTest(httpPort = 1111)
+class ContributionFilesServiceTest {
+    private static final LocalDate startPeriod = LocalDate.of(2023, 1, 1);
+    private static final LocalDate finishPeriod = LocalDate.of(2023, 1, 31);
+
+    @Autowired
+    ContributionFilesReportService contributionFilesReportService;
+
+
+    @Test
+    void givenValidDateLimitParams_whenGetContributionFilesIsInvoked_thenResponseDataModelIsReturned()  throws WebClientResponseException {
+        ContributionFilesResponse result = contributionFilesReportService.getContributionFiles(startPeriod, finishPeriod);
+
+        assertNotNull(result);
+        assertEquals(2, result.getFiles().size());
+        assertTrue(result.getFiles().get(0).contains("id=\"222772044"));
+        assertTrue(result.getFiles().get(1).contains("id=\"222772045"));
+    }
+
+    @Test
+    void givenInternalServerError_whenGetContributionFilesIsInvoked_thenHttpServerErrorExceptionIsThrown(){
+        // setup
+        LocalDate date = LocalDate.of(1500, 5, 5);
+
+        // execute
+        Exception exception = assertThrows(HttpServerErrorException.class,
+                () -> contributionFilesReportService.getContributionFiles(date, date));
+
+        String expectedMessage = "500 Received error 500";
+        String actualMessage = exception.getMessage();
+        // assert
+        assertTrue(actualMessage.contains(expectedMessage));
+    }
+
+    @Test
+    void givenNotFoundServerError_whenGetContributionFilesIsInvoked_thenWebClientResponseExceptionIsThrown(){
+        // setup
+        LocalDate date = LocalDate.of(1404, 4, 4);
+
+        // execute
+        Exception exception = assertThrows(WebClientResponseException.class,
+                () -> contributionFilesReportService.getContributionFiles(date, date)
+        );
+
+        String expectedMessage = "404 Not Found";
+        String actualMessage = exception.getMessage();
+        // assert
+        assertTrue(actualMessage.contains(expectedMessage));
+    }
+
+    @Test
+    void givenServerError_whenGetContributionFilesIsInvoked_thenMaatApiClientExceptionIsThrown(){
+        // setup
+        LocalDate date = LocalDate.of(1400, 4, 4);
+
+        // execute
+        Exception exception = assertThrows(MaatApiClientException.class,
+                () -> contributionFilesReportService.getContributionFiles(date, date)
+        );
+
+        String expectedMessage = "Received error 400";
+        String actualMessage = exception.getMessage();
+        // assert
+        assertTrue(actualMessage.contains(expectedMessage));
+    }
+}
