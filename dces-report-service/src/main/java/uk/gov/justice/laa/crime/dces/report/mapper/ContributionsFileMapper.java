741417b77f484ea76449a9bf85a9ffe6bf151697
Dave Edwards <dave.edwards@digital.justice.gov.uk>
2023-07-14 16:08:05 +0100

    Fixing whitespace.

--- dces-report-service/src/main/java/uk/gov/justice/laa/crime/dces/report/mapper/ContributionsFileMapper.java
+++ dces-report-service/src/main/java/uk/gov/justice/laa/crime/dces/report/mapper/ContributionsFileMapper.java
@@ -64,22 +64,22 @@ public class ContributionsFileMapper {
 
     public CSVDataLine buildCSVDataLine(CONTRIBUTIONS contribution, LocalDate startDate, LocalDate endDate){
         // Business logic inside several of these. Go to methods for details.
-            return CSVDataLine.builder()
-                    .maatId(getMaatId(contribution))
-                    .dataFeedType(getDataFeed(contribution))
-                    // Uses start and end date to control visibility. If a date entry is outside of the range, it is not used.
-                    // E.g. hardshipReviewDate is 01/01/2023, and the start/end dates are 01/10/2022 and 01/11/2022, then it
-                    // would return "", as it lies outside the values.
-                    .assessmentDate(getAssessmentDate(contribution, startDate, endDate))
-                    .repOrderStatusDate(getRepOrderStatusDate(contribution, startDate, endDate))
-                    .hardshipReviewDate(getHardshipReviewDate(contribution, startDate, endDate))
-                    .passportedDate(getPassportedDate(contribution, startDate, endDate))
-                    // filter list of values to only ones where date is in range given.
-                    // Then take the date from the very first one in the list, if exists.
-                    // Otherwise returns ""
-                    .ccOutcomeDate(getOutcomeDate(contribution, startDate, endDate))
-                    .correspondenceSentDate(getCorrespondenceSentDate(contribution, startDate, endDate))
-                    .build();
+        return CSVDataLine.builder()
+                .maatId(getMaatId(contribution))
+                .dataFeedType(getDataFeed(contribution))
+                // Uses start and end date to control visibility. If a date entry is outside of the range, it is not used.
+                // E.g. hardshipReviewDate is 01/01/2023, and the start/end dates are 01/10/2022 and 01/11/2022, then it
+                // would return "", as it lies outside the values.
+                .assessmentDate(getAssessmentDate(contribution, startDate, endDate))
+                .repOrderStatusDate(getRepOrderStatusDate(contribution, startDate, endDate))
+                .hardshipReviewDate(getHardshipReviewDate(contribution, startDate, endDate))
+                .passportedDate(getPassportedDate(contribution, startDate, endDate))
+                // filter list of values to only ones where date is in range given.
+                // Then take the date from the very first one in the list, if exists.
+                // Otherwise returns ""
+                .ccOutcomeDate(getOutcomeDate(contribution, startDate, endDate))
+                .correspondenceSentDate(getCorrespondenceSentDate(contribution, startDate, endDate))
+                .build();
     }
 
 
