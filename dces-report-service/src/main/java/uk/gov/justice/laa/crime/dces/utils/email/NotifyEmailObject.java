62f5eed5612b30679ac0bb45eef7b3aeddba07ae
vloruncg <victor.olorunleye@digital.justice.gov.uk>
2023-07-26 03:22:06 +0100

    WIP
    more refactoring and additional exceptions

--- dces-report-service/src/main/java/uk/gov/justice/laa/crime/dces/utils/email/NotifyEmailObject.java
+++ dces-report-service/src/main/java/uk/gov/justice/laa/crime/dces/utils/email/NotifyEmailObject.java
@@ -2,13 +2,20 @@ package uk.gov.justice.laa.crime.dces.utils.email;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import org.apache.commons.io.FileUtils;
 import uk.gov.justice.laa.crime.dces.utils.email.exceptions.EmailObjectInvalidException;
+import uk.gov.service.notify.NotificationClient;
+import uk.gov.service.notify.NotificationClientException;
 
+import java.io.File;
+import java.io.IOException;
 import java.util.Map;
 
 @AllArgsConstructor
 @Data
 public class NotifyEmailObject implements EmailObject {
+
+    private static String UPLOAD_KEY = "link_to_file";
 
     String templateId;
     String emailAddress;
@@ -26,9 +33,15 @@ public class NotifyEmailObject implements EmailObject {
         if (emailAddress.isEmpty()) {
             throw new EmailObjectInvalidException("email address cannot be empty on email object");
         }
+
+        if (!personalisation.containsKey(UPLOAD_KEY)) {
+            throw new EmailObjectInvalidException("file attachment cannot be empty on email object");
+        }
     }
 
-    public void addAttachment() {
-
+    public void addAttachment(File file) throws IOException, NotificationClientException {
+        // format file for upload
+        byte[] fileContents = FileUtils.readFileToByteArray(file);
+        personalisation.put(UPLOAD_KEY, NotificationClient.prepareUpload(fileContents, true));
     }
 }
