6337e0f15a2306fc21b96b8b44f7e72d84816a30
vloruncg <135148208+vloruncg@users.noreply.github.com>
2023-07-28 09:46:08 +0100

    Notify client integration and email sending utility. (#38)
    * init notifyClient
    
    * WIP
    
    env values and init
    
    * wip
    
    more refactoring added standardised interfaces
    
    * WIP
    
    more refactoring and additional exceptions
    
    * WIP
    
    breaking tests
    
    * integration tests passing
    
    WIP more refactoring
    
    * exclude integration test from build
    
    * more refactoring
    
    adding unhappy path tests, excluding integration tests from build
    
    * fixing code smells
    
    * small refactor and code smell fix
    
    * code coverage improvements
    
    * Update NotifyEmailObjectTest.java

---
+++ dces-report-service/src/test/java/uk/gov/justice/laa/crime/dces/utils/email/NotifyEmailObjectTest.java
@@ -0,0 +1,84 @@
+package uk.gov.justice.laa.crime.dces.utils.email;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestInstance;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ContextConfiguration;
+import uk.gov.justice.laa.crime.dces.utils.email.exceptions.EmailObjectInvalidException;
+
+import java.io.File;
+import java.util.HashMap;
+import java.util.Map;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)
+@ContextConfiguration(classes = NotifyEmailObject.class)
+class NotifyEmailObjectTest {
+
+    private NotifyEmailObject notifyEmailObject;
+
+    @BeforeEach
+    void setup() {
+        Map<String, Object> personalisation = new HashMap<>();
+        notifyEmailObject = new NotifyEmailObject();
+        notifyEmailObject.setEmailAddress("emailAdd");
+        notifyEmailObject.setTemplateId("templateId");
+        notifyEmailObject.setPersonalisation(personalisation);
+        notifyEmailObject.setEmailReplyToId("");
+        notifyEmailObject.setReference("ref");
+    }
+
+    @Test
+    void emailObjectIsValid() {
+        // setup
+        notifyEmailObject.getPersonalisation().put("link_to_file", "linkMissing");
+
+        // execute
+        notifyEmailObject.validate();
+    }
+
+    @Test
+    void testInitEmailObject() {
+        assertEquals("emailAdd", notifyEmailObject.getEmailAddress());
+        assertEquals("templateId", notifyEmailObject.getTemplateId());
+        assertEquals("ref", notifyEmailObject.getReference());
+        assertEquals("", notifyEmailObject.getEmailReplyToId());
+    }
+
+    @Test
+    void emailObjectWithEmailAddressEmptyIsInValid() {
+        // setup
+        notifyEmailObject.getPersonalisation().put("link_to_file", "linkMissing");
+        notifyEmailObject.setEmailAddress("");
+
+        // execute
+        Exception exception = assertThrows(EmailObjectInvalidException.class, () -> notifyEmailObject.validate());
+        assertEquals("email address cannot be empty on email object", exception.getMessage());
+    }
+
+    @Test
+    void emailObjectWithNoAttachmentIsInValid() {
+        Map<String, Object> personalisation = new HashMap<>();
+        NotifyEmailObject emailObject = new NotifyEmailObject(
+                "id",
+                "email",
+                personalisation,
+                "voTest",
+                ""
+        );
+        // execute
+        Exception exception = assertThrows(EmailObjectInvalidException.class, emailObject::validate);
+        assertEquals("file attachment cannot be empty on email object", exception.getMessage());
+    }
+
+    @Test
+    void addAttachmentSuccess() throws Exception {
+        ClassLoader classLoader = getClass().getClassLoader();
+        File file = new File(classLoader.getResource("testContributionReport.csv").getFile());
+
+        assertDoesNotThrow(() -> notifyEmailObject.addAttachment(file));
+    }
+}
