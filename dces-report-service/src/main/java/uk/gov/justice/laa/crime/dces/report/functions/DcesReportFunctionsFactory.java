bf2883cfc358c9d5d88ff1b9a67389361cc67924
Tomas <tomas.novellino@digital.justice.gov.uk>
2023-07-28 17:02:56 +0100

    Updated scheduler
    test coverage pending

--- dces-report-service/src/main/java/uk/gov/justice/laa/crime/dces/report/functions/DcesReportFunctionsFactory.java
+++ dces-report-service/src/main/java/uk/gov/justice/laa/crime/dces/report/functions/DcesReportFunctionsFactory.java
@@ -8,14 +8,14 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.messaging.Message;
 import org.springframework.messaging.MessageHeaders;
-import uk.gov.justice.laa.crime.dces.report.service.DcesReportServiceImpl;
+import uk.gov.justice.laa.crime.dces.report.service.DcesReportService;
+import uk.gov.justice.laa.crime.dces.utils.DateUtils;
 
 import java.io.File;
 import java.io.IOException;
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
 import java.util.function.Consumer;
-import java.util.function.Supplier;
 
 @Configuration
 @RequiredArgsConstructor
@@ -24,16 +24,16 @@ public class DcesReportFunctionsFactory {
     @Value("spring.mvc.format.date")
     private static final String DATE_FORMAT="dd.MM.yyyy";
     private final DateTimeFormatter dateFormatter = DateTimeFormatter.ofPattern(DATE_FORMAT);
-    private final DcesReportServiceImpl reportService;
+    private final DcesReportService reportService;
 
     @Bean
-    public Consumer<Message<String>> contributions() {
-        return (requestMessage) -> {
-            MessageHeaders headers = requestMessage.getHeaders();
-            LocalDate fromDate = convertDateFromHeader("fromDate", headers);
-            LocalDate toDate = convertDateFromHeader("toDate", headers);
+    public Consumer<Message<String>> contributionsReport() {
+        return (request) -> {
+            MessageHeaders headers = request.getHeaders();
+            LocalDate fromDate = convertFromDate(headers);
+            LocalDate toDate = convertToDate(headers);
 
-            log.info("getContributionsReport: between [{}] and [{}]",
+            log.info("contributionsReport: request contribution files between [{}] and [{}]",
                     fromDate.format(dateFormatter),
                     toDate.format(dateFormatter)
             );
@@ -44,18 +44,53 @@ public class DcesReportFunctionsFactory {
                 throw new RuntimeException(e);
             }
         };
-
-    }
-
-    private LocalDate convertDateFromHeader(String key, MessageHeaders headers) {
-        DateTimeFormatter formatter = DateTimeFormatter.ofPattern(DATE_FORMAT);
-        String dateToConvert = headers.getOrDefault(key.toLowerCase(), "").toString();
-        return LocalDate.parse(dateToConvert, formatter);
     }
 
     @Bean
-    public Supplier<String> health() {
-        log.info("Health check requested");
-        return () -> "Alive";
+    public Consumer<Message<String>> fdcReport() {
+        return (request) -> {
+            MessageHeaders headers = request.getHeaders();
+            LocalDate fromDate = convertFromDate(headers);
+            LocalDate toDate = convertToDate(headers);
+
+            log.info("fdcReport: request FDC files between [{}] and [{}]",
+                    fromDate.format(dateFormatter),
+                    toDate.format(dateFormatter)
+            );
+
+            try {
+                File reportFile = reportService.getFdcReport(fromDate, toDate);
+            } catch (JAXBException | IOException e) {
+                throw new RuntimeException(e);
+            }
+        };
+    }
+
+    private boolean validateInputs(MessageHeaders headers) {
+        return (headers.containsKey("fromdate") && headers.containsKey("todate"));
+    }
+
+    private LocalDate convertFromDate(MessageHeaders headers) {
+        return (validateInputs(headers)) ?
+                convertDateFromHeader("fromdate", headers) :
+                    DateUtils.getCurrentFromDate()
+        ;
+    }
+
+    private LocalDate convertToDate(MessageHeaders headers) {
+        return (validateInputs(headers)) ?
+            convertDateFromHeader("todate", headers) :
+                DateUtils.getCurrentToDate()
+        ;
+    }
+
+    private LocalDate convertDateFromHeader(String key, MessageHeaders headers) {
+        if (!headers.containsKey(key))
+            return null;
+
+        return LocalDate.parse(
+                headers.getOrDefault(key, null).toString(),
+                dateFormatter
+        );
     }
 }
---
+++ dces-report-service/src/main/java/uk/gov/justice/laa/crime/dces/report/scheduler/DcesReportScheduler.java
@@ -0,0 +1,49 @@
+package uk.gov.justice.laa.crime.dces.report.scheduler;
+
+import jakarta.xml.bind.JAXBException;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
+import uk.gov.justice.laa.crime.dces.report.service.DcesReportService;
+import uk.gov.justice.laa.crime.dces.utils.DateUtils;
+
+import java.io.File;
+import java.io.IOException;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+
+@Component
+@Slf4j
+@RequiredArgsConstructor
+public class DcesReportScheduler {
+    @Value("spring.mvc.format.date")
+    private static final String DATE_FORMAT="dd.MM.yyyy";
+    private final DateTimeFormatter dateFormatter = DateTimeFormatter.ofPattern(DATE_FORMAT);
+    private final DcesReportService reportService;
+
+    @Scheduled(cron = "${spring.scheduling.contributions.cron}")
+    public void contributionsReport() throws JAXBException, IOException {
+        LocalDate fromDate = DateUtils.getCurrentFromDate();
+        LocalDate toDate = DateUtils.getCurrentToDate();
+
+        log.info("CRON :: contributions: launching between [{}] and [{}]",
+                fromDate.format(dateFormatter),
+                toDate.format(dateFormatter));
+
+//        File reportFile = reportService.getContributionsReport(fromDate, toDate);
+    }
+
+    @Scheduled(cron = "${spring.scheduling.fdc.cron}")
+    public void fdcReport() throws JAXBException, IOException {
+        LocalDate fromDate = DateUtils.getCurrentFromDate();
+        LocalDate toDate = DateUtils.getCurrentToDate();
+
+        log.info("CRON :: FDC: launching between [{}] and [{}]",
+                fromDate.format(dateFormatter),
+                toDate.format(dateFormatter));
+
+//        File reportFile = reportService.getFdcReport(fromDate, toDate);
+    }
+}
