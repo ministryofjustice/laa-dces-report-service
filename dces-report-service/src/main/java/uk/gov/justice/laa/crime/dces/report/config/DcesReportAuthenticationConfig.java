e65c502a025008abe287bcb5ae1a75fe54526063
Tomas <tomas.novellino@digital.justice.gov.uk>
2023-08-03 11:23:07 +0100

    removed unused imports

--- dces-report-service/src/main/java/uk/gov/justice/laa/crime/dces/report/config/DcesReportAuthenticationConfig.java
+++ dces-report-service/src/main/java/uk/gov/justice/laa/crime/dces/report/config/DcesReportAuthenticationConfig.java
@@ -3,7 +3,6 @@ package uk.gov.justice.laa.crime.dces.report.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
-import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
