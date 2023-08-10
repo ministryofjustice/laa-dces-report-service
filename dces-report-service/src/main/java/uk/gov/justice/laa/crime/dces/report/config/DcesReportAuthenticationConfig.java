5715a9d3eb38dc5de08ad8df638bac7dfd46624a
Tomas <tomas.novellino@digital.justice.gov.uk>
2023-08-10 15:02:31 +0100

    Updated Docker file to include CURL command. That way we can actually run the adhoc report from the container. Also copied bash scripts for easier run
    Note the dynamic port part is missing because on the container we don't actually deploy the configuration file. Open for suggestions on this

--- dces-report-service/src/main/java/uk/gov/justice/laa/crime/dces/report/config/DcesReportAuthenticationConfig.java
+++ dces-report-service/src/main/java/uk/gov/justice/laa/crime/dces/report/config/DcesReportAuthenticationConfig.java
@@ -42,8 +42,6 @@ public class DcesReportAuthenticationConfig {
     private boolean isRequestFromLocal(HttpServletRequest request) {
         IpAddressMatcher hasIpAddress = new IpAddressMatcher(LOCALHOST_IPV4);
         IpAddressMatcher hasIp6Address = new IpAddressMatcher(LOCALHOST_IPV6);
-        return hasIpAddress.matches(request) || hasIp6Address.matches(request) ||
-                hasIpAddress.matches(request.getServerName())
-        ;
+        return hasIpAddress.matches(request) || hasIp6Address.matches(request);
     }
 }
