cfe600d425cc70110fb8abd641460b6dd4618ec8
vloruncg <victor.olorunleye@digital.justice.gov.uk>
2023-08-14 12:57:14 +0100

    Update MailerService.java
    adds private constructor

--- dces-report-service/src/main/java/uk/gov/justice/laa/crime/dces/report/service/MailerService.java
+++ dces-report-service/src/main/java/uk/gov/justice/laa/crime/dces/report/service/MailerService.java
@@ -6,6 +6,9 @@ import uk.gov.justice.laa.crime.dces.report.utils.email.EmailObject;
 
 @Component
 public final class MailerService {
+    
+    private MailerService() {
+    }
 
     public static void sendEmail(EmailObject emailObject, EmailClient emailClient) {
         emailClient.send(emailObject);
