f437e6d49b7b88eae295d6b8328dfc3d693d03c7
Tomas Novellino <tomas.novellino@digital.justice.gov.uk>
2023-08-15 10:47:47 +0100

    Added Email Exception response handler and Invalid Date format exception handler

--- dces-report-service/src/main/java/uk/gov/justice/laa/crime/dces/report/exception/DcesExceptionResponseHandler.java
+++ dces-report-service/src/main/java/uk/gov/justice/laa/crime/dces/report/exception/DcesExceptionResponseHandler.java
@@ -5,6 +5,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.method.annotation.MethodArgumentTypeMismatchException;
 
 @ControllerAdvice
 public class DcesExceptionResponseHandler {
@@ -14,5 +15,13 @@ public class DcesExceptionResponseHandler {
             DcesReportSourceFilesDataNotFound exception
     ) {
         return ResponseEntity.status(HttpStatus.NOT_FOUND).body(exception.getMessage());
+    }
+
+    @ExceptionHandler(MethodArgumentTypeMismatchException.class)
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    public ResponseEntity<String> handleMethodArgumentTypeMismatchException(
+            MethodArgumentTypeMismatchException exception
+    ) {
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(exception.getMessage());
     }
 }
---
+++ dces-report-service/src/main/java/uk/gov/justice/laa/crime/dces/report/utils/email/exceptions/EmailExceptionResponseHandler.java
@@ -0,0 +1,20 @@
+package uk.gov.justice.laa.crime.dces.report.utils.email.exceptions;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+
+@ControllerAdvice
+public class EmailExceptionResponseHandler {
+
+    @ExceptionHandler(EmailClientException.class)
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    public ResponseEntity<String> handleEmailClientException(
+            EmailClientException exception
+    ) {
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(exception.getMessage());
+    }
+}
