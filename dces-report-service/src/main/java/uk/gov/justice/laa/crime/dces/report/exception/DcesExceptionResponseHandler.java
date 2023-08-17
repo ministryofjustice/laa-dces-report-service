095763abffc70f132d22156ee7736e8dbf8e9460
Tomas Novellino <tomas.novellino@digital.justice.gov.uk>
2023-08-17 12:48:41 +0100

    Added log for exception message in exception response handlers. Turning sentry debug off

--- dces-report-service/src/main/java/uk/gov/justice/laa/crime/dces/report/exception/DcesExceptionResponseHandler.java
+++ dces-report-service/src/main/java/uk/gov/justice/laa/crime/dces/report/exception/DcesExceptionResponseHandler.java
@@ -1,5 +1,6 @@
 package uk.gov.justice.laa.crime.dces.report.exception;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
@@ -7,6 +8,7 @@ import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.method.annotation.MethodArgumentTypeMismatchException;
 
+@Slf4j
 @ControllerAdvice
 public class DcesExceptionResponseHandler {
     @ExceptionHandler(DcesReportSourceFilesDataNotFound.class)
@@ -14,6 +16,7 @@ public class DcesExceptionResponseHandler {
     public ResponseEntity<String> handleDcesReportSourceFilesDataNotFoundException(
             DcesReportSourceFilesDataNotFound exception
     ) {
+        log.error("{} :: {}", HttpStatus.NOT_FOUND, exception.getMessage());
         return ResponseEntity.status(HttpStatus.NOT_FOUND).body(exception.getMessage());
     }
 
@@ -22,6 +25,7 @@ public class DcesExceptionResponseHandler {
     public ResponseEntity<String> handleMethodArgumentTypeMismatchException(
             MethodArgumentTypeMismatchException exception
     ) {
+        log.error("{} :: {}", HttpStatus.BAD_REQUEST, exception.getMessage());
         return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(exception.getMessage());
     }
 }
