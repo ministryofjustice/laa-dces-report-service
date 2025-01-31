4f779473b985acc57728c9b50d7af6db71c9bf9c
vvartakMoJ <vidyanand.vartak@digital.justice.gov.uk>
2025-01-31 17:41:06 +0000

    More debug to see which bean is missing

--- dces-report-service/src/test/java/uk/gov/justice/laa/crime/dces/report/DcesReportServiceApplicationTests.java
+++ dces-report-service/src/test/java/uk/gov/justice/laa/crime/dces/report/DcesReportServiceApplicationTests.java
@@ -1,8 +1,11 @@
 package uk.gov.justice.laa.crime.dces.report;
 
+import java.util.Arrays;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.ApplicationContext;
 import org.springframework.test.annotation.DirtiesContext;
 import org.springframework.test.context.ActiveProfiles;
 
@@ -11,7 +14,17 @@ import org.springframework.test.context.ActiveProfiles;
 @EnableAutoConfiguration
 @ActiveProfiles("test")
 class DcesReportServiceApplicationTests {
+	@Autowired
+	private ApplicationContext applicationContext;
 
+	@Test
+	void printAllBeans() {
+		String[] beanNames = applicationContext.getBeanDefinitionNames();
+		Arrays.sort(beanNames);
+		for (String beanName : beanNames) {
+			System.out.println(beanName);
+		}
+	}
 	@java.lang.SuppressWarnings("squid:S2699")
 	@Test
 	void contextLoads() {}
