518925979fc3c1c1692d2a9b9bc164ece2b2a2dc
Clement Ibuanokpe <clement.ibuanokpe1@justice.gov.uk>
2025-11-03 17:06:36 +0000

    Add saving methods and unit tests to CaseSubmissionErrorService.

--- dces-report-service/src/main/java/uk/gov/justice/laa/crime/dces/report/service/CaseSubmissionErrorService.java
+++ dces-report-service/src/main/java/uk/gov/justice/laa/crime/dces/report/service/CaseSubmissionErrorService.java
@@ -57,4 +57,58 @@ public class CaseSubmissionErrorService {
     
     return dtos;
   }
+
+  public CaseSubmissionErrorDto saveCaseSubmissionError(CaseSubmissionErrorDto dto) {
+    CaseSubmissionErrorEntity entity = mapDtoToEntity(dto);
+
+    CaseSubmissionErrorEntity returnedEntity = caseSubmissionErrorRepository.save(entity);
+
+    return mapEntityToDto(returnedEntity);
+  }
+
+  public List<CaseSubmissionErrorDto> saveCaseSubmissionErrors(List<CaseSubmissionErrorDto> dtos) {
+    List<CaseSubmissionErrorEntity> entities = new ArrayList<>();
+    for (CaseSubmissionErrorDto dto : dtos) {
+      CaseSubmissionErrorEntity entity = mapDtoToEntity(dto);
+      entities.add(entity);
+    }
+
+    List<CaseSubmissionErrorEntity> returnedEntities = caseSubmissionErrorRepository.saveAll(entities);
+
+    List<CaseSubmissionErrorDto> returnDtos = new ArrayList<>();
+    for (CaseSubmissionErrorEntity entity : returnedEntities) {
+      CaseSubmissionErrorDto dto = mapEntityToDto(entity);
+      returnDtos.add(dto);
+    }
+
+    return returnDtos;
+  }
+
+  private CaseSubmissionErrorEntity mapDtoToEntity(CaseSubmissionErrorDto dto) {
+
+    CaseSubmissionErrorEntity entity = new CaseSubmissionErrorEntity();
+    entity.setMaatId(dto.getMaatId());
+    entity.setConcorContributionId(dto.getConcorContributionId());
+    entity.setFdcId(dto.getFdcId());
+    entity.setTitle(dto.getTitle());
+    entity.setStatus(dto.getStatus());
+    entity.setDetail(dto.getDetail());
+    entity.setCreationDate(dto.getCreationDate());
+
+    return entity;
+  }
+
+  private CaseSubmissionErrorDto mapEntityToDto(CaseSubmissionErrorEntity entity) {
+
+    return CaseSubmissionErrorDto.builder()
+        .id(entity.getId())
+        .maatId(entity.getMaatId())
+        .concorContributionId(entity.getConcorContributionId())
+        .fdcId(entity.getFdcId())
+        .title(entity.getTitle())
+        .status(entity.getStatus())
+        .detail(entity.getDetail())
+        .creationDate(entity.getCreationDate())
+        .build();
+  }
 }
--- dces-report-service/src/test/java/uk/gov/justice/laa/crime/dces/report/service/CaseSubmissionErrorServiceTest.java
+++ dces-report-service/src/test/java/uk/gov/justice/laa/crime/dces/report/service/CaseSubmissionErrorServiceTest.java
@@ -1,5 +1,7 @@
 package uk.gov.justice.laa.crime.dces.report.service;
 
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.anyIterable;
 import static org.mockito.Mockito.when;
 
 import java.time.LocalDateTime;
@@ -32,18 +34,29 @@ public class CaseSubmissionErrorServiceTest {
   private SoftAssertions softly;
 
   private List<CaseSubmissionErrorEntity> entityList;
+  private List<CaseSubmissionErrorDto> dtoList;
 
   @BeforeEach
   void setUp() {
+    dtoList = List.of(
+        CaseSubmissionErrorDto.builder().maatId(1).concorContributionId(1).fdcId(1).title("error title 1").status(1).detail("error detail 1").creationDate(LocalDateTime.of(2025, 1, 1, 11, 10, 0)).build(),
+        CaseSubmissionErrorDto.builder().maatId(2).concorContributionId(2).fdcId(2).title("error title 2").status(2).detail("error detail 2").creationDate(LocalDateTime.of(2025, 2, 2, 11, 10, 0)).build(),
+        CaseSubmissionErrorDto.builder().maatId(3).concorContributionId(3).fdcId(3).title("error title 3").status(3).detail("error detail 3").creationDate(LocalDateTime.of(2025, 3, 3, 11, 10, 0)).build(),
+        CaseSubmissionErrorDto.builder().maatId(4).concorContributionId(4).fdcId(4).title("error title 4").status(4).detail("error detail 4").creationDate(LocalDateTime.of(2025, 4, 4, 11, 10, 0)).build(),
+        CaseSubmissionErrorDto.builder().maatId(5).concorContributionId(5).fdcId(5).title("error title 5").status(5).detail("error detail 5").creationDate(LocalDateTime.of(2025, 5, 5, 11, 10, 0)).build()
+    );
+
     entityList = List.of(
         new CaseSubmissionErrorEntity(1, 1, 1, 1, "error title 1", 1, "error detail 1", LocalDateTime.of(2025, 1, 1, 11, 10, 0)),
         new CaseSubmissionErrorEntity(2, 2, 2, 2, "error title 2", 2, "error detail 2", LocalDateTime.of(2025, 2, 2, 11, 10, 0)),
-        new CaseSubmissionErrorEntity(3, 3, 3, 3, "error title 3", 3, "error detail 3", LocalDateTime.of(2025, 3, 3, 11, 10, 0))
+        new CaseSubmissionErrorEntity(3, 3, 3, 3, "error title 3", 3, "error detail 3", LocalDateTime.of(2025, 3, 3, 11, 10, 0)),
+        new CaseSubmissionErrorEntity(4, 4, 4, 4, "error title 4", 4, "error detail 4", LocalDateTime.of(2025, 4, 4, 11, 10, 0)),
+        new CaseSubmissionErrorEntity(5, 5, 5, 5, "error title 5", 5, "error detail 5", LocalDateTime.of(2025, 5, 5, 11, 10, 0))
     );
   }
 
   @Test
-  public void givenCaseSubmissionErrorEntities_whenGetCaseSubmissionErrorEntityWithEntityId_thenReturnCorrectCaseSubmissionErrorDto() {
+  public void givenValidEntityId_whenGetCaseSubmissionErrorEntity_thenReturnCorrectCaseSubmissionErrorDto() {
     when(caseSubmissionErrorRepository.findById(1)).thenReturn(Optional.of(entityList.getFirst()));
 
     CaseSubmissionErrorDto dto = caseSubmissionErrorService.getCaseSubmissionErrorEntity(1);
@@ -59,12 +72,12 @@ public class CaseSubmissionErrorServiceTest {
   }
 
   @Test
-  public void givenCaseSubmissionErrorEntities_whenGetCaseSubmissionErrors_thenReturnAllCaseSubmissionErrorDtos() {
+  public void whenGetCaseSubmissionErrors_thenReturnAllCaseSubmissionErrorDtos() {
     when(caseSubmissionErrorRepository.findAll()).thenReturn(entityList);
 
     List<CaseSubmissionErrorDto> dtos = caseSubmissionErrorService.getCaseSubmissionErrors();
 
-    softly.assertThat(dtos).hasSize(3);
+    softly.assertThat(dtos).hasSize(5);
     softly.assertThat(dtos.getFirst().getId()).isEqualTo(1);
     softly.assertThat(dtos.getFirst().getMaatId()).isEqualTo(1);
     softly.assertThat(dtos.getFirst().getConcorContributionId()).isEqualTo(1);
@@ -82,6 +95,48 @@ public class CaseSubmissionErrorServiceTest {
     softly.assertThat(dtos.get(2).getStatus()).isEqualTo(3);
     softly.assertThat(dtos.get(2).getDetail()).isEqualTo("error detail 3");
     softly.assertThat(dtos.get(2).getCreationDate()).isEqualTo(LocalDateTime.of(2025, 3, 3, 11, 10, 0));
+  }
+
+  @Test
+  public void givenCaseSubmissionErrorDto_whenSaveCaseSubmissionError_thenReturnCorrespondingSavedCaseSubmissionErrorDto() {
+    when(caseSubmissionErrorRepository.save(any())).thenReturn(entityList.getLast());
+
+    CaseSubmissionErrorDto dto = caseSubmissionErrorService.saveCaseSubmissionError(dtoList.getLast());
+
+    softly.assertThat(dto.getId()).isEqualTo(5);
+    softly.assertThat(dto.getMaatId()).isEqualTo(5);
+    softly.assertThat(dto.getConcorContributionId()).isEqualTo(5);
+    softly.assertThat(dto.getFdcId()).isEqualTo(5);
+    softly.assertThat(dto.getTitle()).isEqualTo("error title 5");
+    softly.assertThat(dto.getStatus()).isEqualTo(5);
+    softly.assertThat(dto.getDetail()).isEqualTo("error detail 5");
+    softly.assertThat(dto.getCreationDate()).isEqualTo(LocalDateTime.of(2025, 5, 5, 11, 10, 0));
+  }
+
+  @Test
+  public void givenCaseSubmissionErrorDtos_whenSaveCaseSubmissionErrors_thenReturnCorrespondingSavedCaseSubmissionErrorDtos() {
+    when(caseSubmissionErrorRepository.saveAll(anyIterable())).thenReturn(entityList);
+
+    List<CaseSubmissionErrorDto> dtos = caseSubmissionErrorService.saveCaseSubmissionErrors(dtoList);
+
+    softly.assertThat(dtos).hasSize(5);
+    softly.assertThat(dtos.getFirst().getId()).isEqualTo(1);
+    softly.assertThat(dtos.getFirst().getMaatId()).isEqualTo(1);
+    softly.assertThat(dtos.getFirst().getConcorContributionId()).isEqualTo(1);
+    softly.assertThat(dtos.getFirst().getFdcId()).isEqualTo(1);
+    softly.assertThat(dtos.getFirst().getTitle()).isEqualTo("error title 1");
+    softly.assertThat(dtos.getFirst().getStatus()).isEqualTo(1);
+    softly.assertThat(dtos.getFirst().getDetail()).isEqualTo("error detail 1");
+    softly.assertThat(dtos.getFirst().getCreationDate()).isEqualTo(LocalDateTime.of(2025, 1, 1, 11, 10, 0));
+
+    softly.assertThat(dtos.get(4).getId()).isEqualTo(5);
+    softly.assertThat(dtos.get(4).getMaatId()).isEqualTo(5);
+    softly.assertThat(dtos.get(4).getConcorContributionId()).isEqualTo(5);
+    softly.assertThat(dtos.get(4).getFdcId()).isEqualTo(5);
+    softly.assertThat(dtos.get(4).getTitle()).isEqualTo("error title 5");
+    softly.assertThat(dtos.get(4).getStatus()).isEqualTo(5);
+    softly.assertThat(dtos.get(4).getDetail()).isEqualTo("error detail 5");
+    softly.assertThat(dtos.get(4).getCreationDate()).isEqualTo(LocalDateTime.of(2025, 5, 5, 11, 10, 0));
   }
 
 }
